2860caa26cb6e5d2836896d62d27acebded0f169
vineethkartha <vineeth.kartha@gmail.com>
2015-03-10 13:42:37 +0530

    Add Loader to dailyVocabulary

--- DailyVocabulary/dailyVocabulary.py
+++ DailyVocabulary/dailyVocabulary.py
@@ -22,6 +22,8 @@ GtkHeaderBar{
 class MyWidget(Gtk.Window):
 	
 	def __init__(self):
+		self._flag=0
+		
 		Gtk.Window.__init__(self,title="Word for the day")
 		self.resize(300,200)
 		self.set_icon_from_file('/media/E/my_works/desktopWidgets/DailyVocabulary/pic/vocab-icon.jpg')
@@ -30,10 +32,13 @@ class MyWidget(Gtk.Window):
 		self.box=Gtk.Box(spacing=2)
 		self.add(self.box)
 		
-		self.label=Gtk.Label("Error Loading Word")
+		self.label=Gtk.Label()
 		self.box.pack_start(self.label,True,True,0)
 		
-		self.loader=Gtk.Spinner()
+		self._loader=Gtk.Spinner()
+		self.box.pack_start(self._loader,True,True,0)
+		self._loader.start()
+		
 		self.header=Gtk.HeaderBar()
 		self.header.set_show_close_button(False)
 		self.header.set_title("Word for the day")
@@ -43,15 +48,16 @@ class MyWidget(Gtk.Window):
 		self.connect("delete-event", Gtk.main_quit)
 		#self.connect("button_press_event", self.button_press_event)
 		#self.newWord()
+		
 		self.cssprovider = Gtk.CssProvider()
 		self.cssprovider.load_from_data(CSScool)
 		screen = Gdk.Screen.get_default()
 		sc = Gtk.StyleContext()
 		sc.add_provider_for_screen(screen, self.cssprovider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 		
-		self.timeout_id = GObject.timeout_add(5000, self.newWord, None)
+		self.timeout_id = GObject.timeout_add(5000, self.newWord)
 	
-	def newWord(self,something):
+	def newWord(self):
 		r=requests.get(url);
 		data=r.text
 		soup=BeautifulSoup(data);
@@ -61,15 +67,22 @@ class MyWidget(Gtk.Window):
 			dispText=dispText+''.join(heading.contents)
 		for divs in soup.find_all('div',style="margin-left: 20px;"):
 			if(count>1):
+				self._flag=1
 				break;
 			count=count+1
 			dispText=dispText+divs.getText();
 			self.label.set_text(dispText)
+		if(self._flag==1):
+			self._loader.stop()
+			GObject.source_remove(self.timeout_id)
 		return True
+		
 	def button_press_event(self,widget,event):
 		if(event.button==3):
 			print"right click"
 		return True
+		
+		
 win = MyWidget()
 win.show_all()
 Gtk.main()
